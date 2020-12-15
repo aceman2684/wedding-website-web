5fced17045f8a7359f774d0ba4cd4d77d8807bed
aceman2684 <andrew.edwards448@gmail.com>
2020-12-14 18:53:21 -0600

    Added react-bootstrap and themes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Container />
+    </div>
+  );
+}
+
+export default App;
