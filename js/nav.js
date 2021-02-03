4f36ebecc4f107b549e223be67ec04f37bbde990
Conor Edwards <andrew.edwards448@gmail.com>
2021-02-03 01:11:15 -0600

    Added hero image for mobile

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,16 @@
-document.addEventListener("mouseup", function (e) {
+function clickOutsideNavEventListener(e) {
   const navbar = document.getElementById("navbar");
   if (!navbar.contains(e.target)) {
     closeNav();
   }
-});
+}
 
 function openNav() {
   document.getElementById("navbar").classList.add("navbar--show");
+  document.addEventListener("mouseup", clickOutsideNavEventListener);
 }
 
 function closeNav() {
   document.getElementById("navbar").classList.remove("navbar--show");
+  document.removeEventListener("mouseup", clickOutsideNavEventListener);
 }
